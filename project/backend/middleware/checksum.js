ef344835213d7a01db5350a59b1e57378ebef80e
Fredrik E <fredrik.ekloof@gmail.com>
2018-12-16 15:52:45 +0100

    Validate that JSON is sent as data payload

--- project/backend/middleware/checksum.js
+++ project/backend/middleware/checksum.js
@@ -2,11 +2,17 @@ const md5checksum = require('../common/md5checksum')
 
 module.exports = (packet, next) => {
   const [_event, dataJSON] = packet
-  const data = JSON.parse(dataJSON)
+  let data = null
+  
+  try {
+    data = JSON.parse(dataJSON)
+  } catch (error) {
+    return next(new Error('Data is not valid JSON'))
+  }
 
   if (!md5checksum.isValidChecksum(data)) {
     console.error('Invalid checksum')
-    return next(new Error('Invalid checksum.'))
+    return next(new Error('Invalid checksum'))
   }
 
   packet[1] = md5checksum.getData(data)
